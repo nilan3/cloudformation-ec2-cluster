b997c092ffa9b64856651832095e7f84868d6428
nilan3 <nbalachandran@nbalachandran.local>
2020-02-05 20:52:24 +0000

    network acls disabled

--- scripts/cfn-deploy.sh
+++ scripts/cfn-deploy.sh
@@ -3,7 +3,8 @@
 ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
 ENVIRONMENT="dev"
 REGION="eu-west-2"
-EC2_KEY_NAME="personal-key"
+EC2_KEY_NAME="personal-aws-key"
+EC2_AMI_ID="ami-0089b31e09ac3fffc"
 
 ACTION=$1
 
@@ -14,7 +15,6 @@ then
 	  --stack-name cfn-setup-stack \
 	  --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM CAPABILITY_AUTO_EXPAND \
 	  --region $REGION \
-	  --parameter-overrides Ec2KeyName=${EC2_KEY_NAME} \
 	  --no-execute-changeset
 elif [[ $ACTION == "deploy" ]]
 then
@@ -25,7 +25,7 @@ then
 	  --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM CAPABILITY_AUTO_EXPAND \
 	  --role-arn arn:aws:iam::${ACCOUNT_ID}:role/AWSCloudFormationServiceRole \
 	  --region $REGION \
-	  --parameter-overrides Ec2KeyName=${EC2_KEY_NAME} \
+	  --parameter-overrides Ec2KeyName=${EC2_KEY_NAME} Ec2AmiId=${EC2_AMI_ID} \
 	  --no-execute-changeset
 else
 	echo "Invalid argument"
